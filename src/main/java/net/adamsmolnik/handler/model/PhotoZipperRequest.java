7bcc294193d07c6b46f6f83e3cf1c3a2bd1101c3
Adam Smolnik <smolnik@wp.pl>
2015-08-04 00:34:42 +0200

    irrelevant details just to make text formatting better

--- src/main/java/net/adamsmolnik/handler/model/PhotoZipperRequest.java
+++ src/main/java/net/adamsmolnik/handler/model/PhotoZipperRequest.java
@@ -10,13 +10,14 @@ public class PhotoZipperRequest extends PhotoRequest {
 
 	public String toDate;
 
+	public PhotoZipperRequest() {
+		
+	}
+
 	public PhotoZipperRequest(String principalId, String fromDate, String toDate) {
 		super(principalId);
 		this.fromDate = fromDate;
 		this.toDate = toDate;
-	}
-
-	public PhotoZipperRequest() {
 	}
 
 }
