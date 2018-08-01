001cfd1d4c7f2df6358c096bdc22bc9bf4ae450f
Adam Smolnik <adam_smolnik@epam.com>
2018-08-01 15:10:50 +0200

    Add aws x-ray handling

--- src/main/java/net/adamsmolnik/handler/PhotoCollectionHandler.java
+++ src/main/java/net/adamsmolnik/handler/PhotoCollectionHandler.java
@@ -5,7 +5,8 @@ import java.util.Comparator;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Index;
 import com.amazonaws.services.dynamodbv2.document.Item;
@@ -13,6 +14,8 @@ import com.amazonaws.services.dynamodbv2.document.ItemCollection;
 import com.amazonaws.services.dynamodbv2.document.QueryOutcome;
 import com.amazonaws.services.dynamodbv2.document.RangeKeyCondition;
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.xray.AWSXRay;
+import com.amazonaws.xray.handlers.TracingHandler;
 
 import net.adamsmolnik.handler.api.model.PhotoCollectionRequest;
 import net.adamsmolnik.handler.api.model.PhotoCollectionResponse;
@@ -24,6 +27,9 @@ import net.adamsmolnik.handler.api.model.PhotoItem;
  */
 public class PhotoCollectionHandler extends PhotoHandler {
 
+	private final AmazonDynamoDB dbClient = AmazonDynamoDBClientBuilder.standard()
+			.withRequestHandlers(new TracingHandler(AWSXRay.getGlobalRecorder())).build();
+
 	public PhotoCollectionResponse handle(PhotoCollectionRequest request, Context context) {
 		long then = System.currentTimeMillis();
 		validateRequest(request);
@@ -32,16 +38,17 @@ public class PhotoCollectionHandler extends PhotoHandler {
 		String principalId = request.getPrincipalId();
 		log.log("Request for " + ptDate + " received");
 
-		DynamoDB db = new DynamoDB(new AmazonDynamoDBClient());
+		DynamoDB db = new DynamoDB(dbClient);
 		Index index = db.getTable("photos").getIndex("photoTakenDate-index");
 		ItemCollection<QueryOutcome> items = index.query(newUserIdentityKeyAttribute(principalId),
 				new RangeKeyCondition("photoTakenDate").eq(ptDate));
 		log.log(then, "QueryOutcome received for " + ptDate);
 		PhotoCollectionResponse response = new PhotoCollectionResponse(ptDate,
 				StreamSupport.stream(items.spliterator(), false)
-						.map(item -> new PhotoItem(item.getString("bucket"), item.getString("photoKey"), item.getString("thumbnailKey"),
-								item.getString("photoTakenDate") + " " + item.getString("photoTakenTime"), item.getString("srcPhotoName"),
-								getOptionalItems(item, "madeBy", "model")))
+						.map(item -> new PhotoItem(item.getString("bucket"), item.getString("photoKey"),
+								item.getString("thumbnailKey"),
+								item.getString("photoTakenDate") + " " + item.getString("photoTakenTime"),
+								item.getString("srcPhotoName"), getOptionalItems(item, "madeBy", "model")))
 						.sorted(Comparator.comparing(PhotoItem::getPhotoKey)).collect(Collectors.toList()));
 		log.log(then, getClass().getSimpleName() + " is about to complete");
 		return response;
